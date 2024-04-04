4771c3f8318aa42635d256beff62d0ed69058d21
hug0-cstrs <hugo.casteras@outlook.fr>
2024-04-04 20:38:44 +0200

    update file add comments for a better understanding

--- utils/redis.js
+++ utils/redis.js
@@ -1,32 +1,34 @@
-import { promisify } from 'util';
-
 const redis = require('redis');
+const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient();
-    this.client.on('error', (error) => console.log(error.message));
+    this.client = redis.createClient(); // create new client
+    this.client.on('error', (error) => { // handle error
+      console.error(error);
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client); // promisify get method
   }
 
-  isAlive() {
+  isAlive() { // check if client is connected
     return this.client.connected;
   }
 
-  async get(key) {
-    const getval = await promisify(this.client.get).bind(this.client);
-    const val = await getval(key);
+  async get(key) { // get value from key
+    const val = await this.getAsync(key);
     return val;
   }
 
-  async set(key, val, duration) {
-    await this.client.set(key, val);
-    await this.client.expire(key, duration);
+  async set(key, value, duration) { // set key value pair with duration
+    this.client.set(key, value);
+    this.client.expire(key, duration);
   }
 
-  async del(key) {
-    await this.client.del(key);
+  async del(key) { // delete key
+    this.client.del(key);
   }
 }
 
-const redisClient = new RedisClient();
+const redisClient = new RedisClient(); // create new instance of RedisClient
 module.exports = redisClient;
